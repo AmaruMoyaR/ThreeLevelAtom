29a48773f58435e9649ae371e32c628c515b0fc7
AmaruMoyaR <amaru.moya.ragal@gmail.com>
2023-09-10 15:39:02 -0300

    first run

--- Hamiltonian.py
+++ Hamiltonian.py
@@ -34,3 +34,23 @@ def Hamiltonian_2levels(psi_inic, N , t = 300, w0 = 1. , w = 1., lamda = 1):
     tasa_inversion = q.expect(sig_z, evolucion_temporal_estado)
     
     return evolucion_temporal_estado, tiempo, tasa_inversion
+
+def Hamiltonian3LevelsLadder(psi_inic, N, t, chi, eta):
+    
+    a_campo = q.tensor(q.identity(3),q.destroy(N))
+    
+    
+    Delta_l = np.abs(omega_12) - omega
+    Delta_r = np.ans(omega_23) - omega
+    
+    H_p = chi* a_campo * b2_electron.dag() * b1_electron + eta * a_campo * b3_electron.dag() * b2_electron + chi* a_campo.dag() * b2_electron * b1_electron.dag() + eta * a_campo.dag() * b3_electron * b2_electron.dag()
+    
+    P_e = b1_electron.dag() * b1_electron + b2_electron.dag() * b2_electron + b3_electron.dag() * b3_electron
+    
+    N_hat = a_campo.dag() * a_campo + b3_electron.dag() * b3_electron - b1_electron.dag() * b1_electron + q.eye(N)
+    
+    H_i = omega * (N_hat - 1) + (omega_2 - omega) * P_e
+    
+    H_ii = - Delta_l * b1_electron.dag() * b1_electron + Delta_r * b2_electron.dag() * b2_electron
+    
+    H = H_p + H_i + H_ii
